7b46b51ab80f0b1fdfaf91ff7436fdb8a521e9ad
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-23 20:01:23 +0900

    URLに直接入力で表示内容を変える

---
+++ webServerUrl.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const http = require("http"); //httpモジュール読み込み
+const dt = require('./DateModule'); //独自モジュール読み込み
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; //200はすべて問題ないことを意味している
+    res.setHeader('Content-Type', 'text/html'); //httpサーバーからの応答がHTMLとして表示させる場合は「text/html」と正しいコンテンスタイプのhttpヘッダーを含める必要がある。
+    res.end(req.url); //URLの最後にsummerを入れると「/summer」と表示される
+});
+
+server.listen(port, hostname, () => {
+    console.log(`${dt.myDateTime()} Server running at http://${hostname}:${port}/`);
+});
