7efe527c97919f95770ceccc33f94f23b2c7ec84
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-23 20:00:43 +0900

    Webサーバ作成

--- webServerHello.js
+++ webServerHello.js
@@ -7,9 +7,9 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Node');
+    res.statusCode = 200; //200はすべて問題ないことを意味している
+    res.setHeader('Content-Type', 'text/html'); //httpサーバーからの応答がHTMLとして表示させる場合は「text/html」と正しいコンテンスタイプのhttpヘッダーを含める必要がある。
+    res.end('Hello Node!');
 });
 
 server.listen(port, hostname, () => {
