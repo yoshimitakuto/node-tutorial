4240fd990f0a18db743400e28b7ed0d277337621
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-23 20:50:14 +0900

    nodejs(express)ルーティング設定

---
+++ node-project/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+/* ルーティング設定の基本構成
+ app.get('アクセスするURL', (req, res) => {
+     URLにアクセルした際の挙動を記載
+}); */
+
+// index.ejsファイルのルーティング設定
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+// test.ejsファイルのルーティング設定
+app.get('/test', (req, res) => {
+    res.render('test.ejs');
+});
+
+// test.ejsファイルのルーティング設定
+app.get('/top', (req, res) => {
+    res.render('top.ejs');
+});
+
+app.listen(3000);
--- webServerUrl.js
+++ webServerUrl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const http = require("http"); //httpモジュール読み込み
+const url = require('url');
 const dt = require('./DateModule'); //独自モジュール読み込み
 
 const hostname = '127.0.0.1';
@@ -9,7 +10,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200; //200はすべて問題ないことを意味している
     res.setHeader('Content-Type', 'text/html'); //httpサーバーからの応答がHTMLとして表示させる場合は「text/html」と正しいコンテンスタイプのhttpヘッダーを含める必要がある。
-    res.end(req.url); //URLの最後にsummerを入れると「/summer」と表示される
+    const q = url.parse(req.url, true).query;
+    const res = q.year + " " + q.month;
+    res.end(res);
+    // res.end(req.url); //URLの最後にsummerを入れると「/summer」と表示される
 });
 
 server.listen(port, hostname, () => {
