49ba3c315bc596aebb03568d501dd2dd8fdad398
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-25 12:44:51 +0900

    readFile（ファイル読み込み）

---
+++ FsModule-Practice/Read.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const http = require('http');
+
+http.createServer(function (req, res) {
+    fs.readFile('demo1.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }).listen(8080);
